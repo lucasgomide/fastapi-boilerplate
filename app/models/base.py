02fb2b68f4cf15f4e1677181dafae45ba81406a0
lucasgomide <lucaslg200@gmail.com>
2024-12-23 12:31:42 -0300

    refactor: replace utcnow by now(tz=UTC)
    The datetime.datetime.utcnow() is deprecated and scheduled for removal in a future version

--- app/models/base.py
+++ app/models/base.py
@@ -1,6 +1,14 @@
 from sqlmodel import Field, SQLModel
+from datetime import datetime, UTC
+from sqlalchemy import event
+
 
 class BaseModel(SQLModel):
     id: int = Field(primary_key=True)
-    created_at: datetime = Field(default_factory=datetime.utcnow)
-    updated_at: datetime | None = Field(default_factory=datetime.utcnow)
+    created_at: datetime = Field(default_factory=lambda: datetime.now(tz=UTC))
+    updated_at: datetime | None = Field(default_factory=lambda: datetime.now(tz=UTC))
+
+
+@event.listens_for(BaseModel, "before_update")
+def set_updated_at(mapper, connection, target):
+    target.updated_at = datetime.now(tz=UTC)
