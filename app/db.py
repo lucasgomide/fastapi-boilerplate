919d94f1a61e7ae93c6b0358210b730ebbf41ae4
lucasgomide <lucaslg200@gmail.com>
2025-01-01 16:26:19 -0300

    build: enhance async session connection

--- app/db.py
+++ app/db.py
@@ -5,9 +5,13 @@ from sqlalchemy import create_engine
 from app.conf import settings
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 
-engine = create_engine(url=settings.DATABASE_URL, echo=True)
+is_development = settings.ENV in ["development", "test"]
 
-async_engine = create_async_engine(url=settings.ASYNC_DATABASE_URL, echo=True)
+engine = create_engine(url=settings.DATABASE_URL, echo=is_development)
+async_engine = create_async_engine(
+    url=settings.ASYNC_DATABASE_URL, 
+    echo=is_development
+)
 
 @contextmanager
 def get_session() -> Generator[Session, None, None]:
@@ -21,6 +25,9 @@ def get_session() -> Generator[Session, None, None]:
     finally:
         session.close()
 
+
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
-    async with sessionmaker(bind=async_engine, expire_on_commit=False, class_=AsyncSession)() as session:
+    async with sessionmaker(
+        bind=async_engine, expire_on_commit=False, class_=AsyncSession
+    )() as session:
         yield session
